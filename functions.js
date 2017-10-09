1a57d876b13b403772ba6a127a1a93b568096f5a
-MMK- <masoumiprojects7@gmail.com>
2017-10-09 21:42:59 +0200

    .

---
+++ functions.js
@@ -0,0 +1,28 @@
+var div = document.getElementById('list');
+	var list = [];
+	var listItem;
+
+
+	function showList(){
+		div.innerHTML = list.join("");
+		document.getElementById("listItem").value = "";
+	}
+
+
+	function clearList(){
+		div.innerHTML = "";
+		list = [];
+	}
+
+
+	function insertItem(listItem){
+
+		var listItem = document.querySelector("#listItem").value;
+		list.push("<li>"+listItem+"</li>");
+		console.log(listItem);
+
+
+		showList();
+
+	}
+
